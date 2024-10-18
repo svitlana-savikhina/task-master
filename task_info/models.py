b7dd51babc5b88bfb15cfccb3e5a8179f9cd3ade
Svitlana Savikhina <savihina111@gmail.com>
2024-10-18 19:26:30 +0300

    Add Task model

---
+++ task_info/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import Column, String, DateTime, Integer
+
+from database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    task_id = Column(String, index=True, unique=True)
+    status = Column(String, default="pending")
+    created_at = Column(DateTime, default=datetime.utcnow)
