160b9a7ce9e2c0af3f4ad840b75b62a7e4142a9a
Svitlana Savikhina <savihina111@gmail.com>
2024-10-22 18:55:52 +0300

    Added logging setup in main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from task_info import routers_ as task_info_router
+from task_info.logging_config import setup_logging
+
+setup_logging("task_master.log")
 
 app = FastAPI()
 
