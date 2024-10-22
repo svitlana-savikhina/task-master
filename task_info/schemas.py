f220131d4b39a68b272d84f4a9f85c55eb8662d7
Svitlana Savikhina <savihina111@gmail.com>
2024-10-22 16:13:48 +0300

    Add validation for task_id field in schema

--- task_info/schemas.py
+++ task_info/schemas.py
@@ -1,5 +1,6 @@
+import re
 from datetime import datetime
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, Field, field_validator
 
 
 class TaskBase(BaseModel):
@@ -17,4 +18,13 @@ class Task(TaskBase):
 
 
 class TaskCreate(TaskBase):
-    task_id: constr(min_length=2, max_length=10)
+    task_id: str = Field(..., description="Only letters and digits allowed, length between 2 and 10")
+
+    @field_validator('task_id')
+    def validate_task_id(cls, value):
+        if not re.match(r'^[A-Za-z0-9]{2,10}$', value):
+            raise ValueError(
+                "Invalid task_id format. Only letters and digits are allowed, and length must be between 2 and 10 "
+                "characters."
+            )
+        return value
